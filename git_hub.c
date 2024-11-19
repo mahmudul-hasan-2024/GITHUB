48efcf3e97343da32ad256d5c2c53b4944197fda
mahmudul_hasan-2024 <mdmahmudulhasan.cse24@gmail.com>
2024-11-19 23:33:18 +0600

    first commit

---
+++ git_hub.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main(){
+    
+    // 1-Pc file name and repository name use are same.
+    // 2-Not add readme.me file...
+
+	// 	***cmd all command
+	// 	(git --version) check version.
+	// 	(C:/D:/E:) change drive.
+	// 	(cd \any name) add folder.
+	// 	(cd any name) add folder inside folder.
+	// 	(cd ..) folder inside folder 1 step back...
+	// 	***
+	// 	**
+	// 	*
+		
+	//   ***vs code terminal cmd all command
+	//   (git config --)
+	//   (git config --global user.name "mahmudul_hasan-2024")
+	//   (git config --global user.email "mdmahmudulhasan.cse24@gmail.com")
+	//   (git config --list)
+	//   (enter,enter and back wq)...
+	//   ***
+	//   **
+	//   *
+	  
+	//   ***git bash all command
+	//   (ls) list check
+	//   (clear) all clear
+	//   (pwd) find drive on pc, defolt drive is C:\...
+	//   ***
+	//   **
+	//   *
+	  
+	//   ***Github create a new repository on the command line...
+	//   echo "# github" >> README.md
+    //   git init
+	// 	git add README.md
+	// 	git commit -m "first commit"
+	// 	git branch -M main
+	// 	git remote add origin https://github.com/mahmudul-hasan-2024/github.git
+	// 	git push -u origin main
+		
+	// 	***next time add any file then
+	// 	git add .
+	// 	git commit -m"added any name"
+	// 	git push
+	// 	***
+	// 	**
+	// 	*
+		
+	// 	***Push an existing repository from the command line
+	// 	git remote add origin https://github.com/mahmudul-hasan-2024/github.git
+	// 	git branch -M main
+	// 	git push -u origin main
+	// 	***
+	// 	**
+	// 	*
+    printf("Hello world!");
+
+    return 0;
+}
